7f25135bda887b35386024f8de3fd74f9c9737eb
Kavitha-2519 <mathikavitha121@gmail.com>
2025-08-04 22:49:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+
+let expression = '';
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.getAttribute('data-value');
+
+    if (value === 'C') {
+      expression = '';
+      display.value = '';
+    } else if (value === '=') {
+      try {
+        expression = eval(expression).toString();
+        display.value = expression;
+      } catch {
+        display.value = 'Error';
+        expression = '';
+      }
+    } else {
+      expression += value;
+      display.value = expression;
+    }
+  });
+});
